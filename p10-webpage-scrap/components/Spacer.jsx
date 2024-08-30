07c7fcb1b778105ae495e583348b2e9a1ec11538
Cheolkie Kim <ckiekim@naver.com>
2024-08-30 13:11:51 +0900

    Webpage scrap

--- p10-webpage-scrap/components/Spacer.jsx
+++
@@ -1,8 +0,0 @@
-import { View } from 'react-native';
-
-export default function Spacer({ space, isHorizontal }) {
-  if (isHorizontal)
-    return <View style={{ marginLeft: space }} />
-
-  return <View style={{ marginTop: space }} />
-}
